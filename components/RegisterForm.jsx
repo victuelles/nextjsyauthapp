49af6c473cbf098730cd71d5267d80ce71c571ac
Rom Victuelles <rom.victuelles@gmail.com>
2023-10-06 12:08:44 -0700

    userinfo added

--- components/RegisterForm.jsx
+++ components/RegisterForm.jsx
@@ -1,17 +1,18 @@
 import Link from "next/link";
 
-export default function LoginForm(){
+export default function RegisterForm(){
     return(
         <div className="grid place-items-center h-screen">
             <div className="shadow-lg p-5 rounder-lg- border-t-4 border-green-400">
-                <h1 className="text-xl font-bold my-4">Login</h1>
+                <h1 className="text-xl font-bold my-4">Register</h1>
                 <form className="flex flex-col gap-3">
+                    <input type="text" placeholder="Full Name"/>
                     <input type="text" placeholder="Email"/>
                     <input type="password" placeholder="Password"/>
                     <button className="bg-green-600 text-white font-bold cursor-pointer px-6 py-2">Login</button>
                      <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">Error message</div>
-                     <Link className="text-sm mt-3 text-right" href={"/register"}>
-                        Don't have an account? <span className="underline">Register</span>
+                     <Link className="text-sm mt-3 text-right" href={"/"}>
+                        Already have an account? <span className="underline">Login</span>
                      </Link>
                 </form>
             </div>
---
+++ components/UserInfo.jsx
@@ -0,0 +1,16 @@
+export default function UserInfo(){
+    return (
+    <div className="grid place-items-center h-screen">
+        <div className="shadow-lg p-8 bg-zinc-300/10 flex flex-col gap-2 my-6">
+            <div>
+                Name: <span className="font-bold">Rom</span>
+            </div>
+            <div>
+                Name: <span className="font-bold">rom@rom.com</span>
+            </div>
+            <button className="bg-red-500 text-white font-bold px-6 py-2 mt-3">Logout</button>
+        </div>
+    
+    </div>
+    )
+}
