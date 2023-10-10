cdea5f80573c64967bc3421ffd16b48a401f823a
Rom Victuelles <rom.victuelles@gmail.com>
2023-10-10 14:42:41 -0700

    mongodb connection added

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -1,11 +1,17 @@
+import { connectMongoDB } from "@/lib/ mongodb"
+import User from "@/models/user"
+import bcrypt from "bcryptjs"
 import { NextResponse } from "next/server"
 
 export async function POST(req){
     try {
         const {name,email,password} = await req.json()
-        console.log("name ",name)
-        console.log("email ",email)
-        console.log("password ",password)
+        // console.log("name ",name)
+        // console.log("email ",email)
+        // console.log("password ",password)
+        const hashedPassword = await bcrypt.hash(password,10)
+        await connectMongoDB()
+        await User.create({name,email,password:hashedPassword})
         return NextResponse.json({message:"User registered"},{status:201})
 
     } catch (error) {
